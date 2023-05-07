4f49a546d50bba6e58190fa78f4388e4acf8f4d7
nikisKuchbhi <69007543+nikisKuchbhi@users.noreply.github.com>
2023-05-07 06:08:28 +0530

    Add files via upload

---
+++ ankilp5/config.js
@@ -0,0 +1,76 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Hey!!",
+        "Happy Birthday",
+        "Thank you!!",
+        "So, Ritik wished you huh",
+        "I am just imagining your reaction",
+        "uh!! bekar ka tumko kiya lgta hain",
+        "hum yeh sab se patt jayega",
+        "bekar ka paisa waste krta hain tum",
+        "humko yehsab ka jarurat nhi",
+        "bro, bro chill!!",
+        "Bro mere kamayi meri marzi kaha kharch kru",
+        "kisike papa se maang kr toh ni kr raha",
+        "jo bhi hain but still ",
+        "maat kiya kroio yehsab",
+        "isse acha bacho ko donate hi krdete",
+        "woh bhi kra hain mila nhi??",
+        "ruko milna toh chahiyeh tha",
+        "hmm heh abb yehbhi kiyu kra ",
+        "hum krne ni keh raha tha",
+        "and bro listen",
+        "yeh agar tu gift dene wala tha na",
+        "toh pehle ake disclose nhi krte",
+        "yaad rakhio next time se",
+        "arehh baba pakki baat",
+        "toh abb sari baat yuhi krni hain",
+        "apne imaginations meh",
+        "ki mujhse bhi baat kroge??",
+        "haaa!! kr raha whatsapp chall",
+        "haa chl chll",
+        "happy birthday to you once again",
+        "thankyou",
+        "tu abhi tak yehi hain gya nhi",
+        "areh ek min ruk le tujhse ",
+        "kuch important baat thi",
+        "bohut bohut important",
+        "kiya",
+        "areeh bohut important",
+        "keh raha hain ki meh jau??",
+        "I Love you bro!!",
+        "haa hogya tera nautanki??",
+        "ha hogya chal abb whatsapp pe intezaar kr raha hu",
+        "ha ha ayi meh ayi",
+
+
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+
+
+    // 
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Most Delicious Cake Ever",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU"
+    },
+
+    // 结束语
+    loveText: 'love you Ankita!!'
+};
